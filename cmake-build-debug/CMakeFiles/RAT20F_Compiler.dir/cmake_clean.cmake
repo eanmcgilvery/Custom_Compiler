a4eef2a8dcbfaf8c95a869144f8f58ae0a19050d
Data Structures for CPSC 131 <eanmcgilvery@gmail.com>
2020-12-07 18:44:55 -0800

    CMAKE FOLDER

---
+++ cmake-build-debug/CMakeFiles/RAT20F_Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RAT20F_Compiler.dir/ObjCodeTable.cpp.o"
+  "CMakeFiles/RAT20F_Compiler.dir/main.cpp.o"
+  "RAT20F_Compiler"
+  "RAT20F_Compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RAT20F_Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
